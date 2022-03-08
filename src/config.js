bdc9420d1a8b49d59c98c785a11a49f5225bcdc6
Abhishek Malviya <71405686+malviya-abhishek@users.noreply.github.com>
2022-03-08 20:57:48 +0530

    url changed

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
  const config=  {
-  // API_URL: "https://my-user-article.herokuapp.com",
-  API_URL: "http://localhost:4000",
+  API_URL: "https://my-user-article.herokuapp.com",
+  // API_URL: "http://localhost:4000",
 };
 
 export default config;
